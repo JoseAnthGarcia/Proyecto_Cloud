0b555e89d5f4bdc9cf4c5a2e4185f2578ad37f5f
Jose - Laptop Acer <garcia.josea@pucp.edu.pe>
2022-06-12 11:36:21 -0500

    Node class added

---
+++ beans/Node.py
@@ -0,0 +1,11 @@
+from email.mime import image
+
+
+class Node:
+    def __init__(self, imagen, flavor, links):
+        self.imagen = imagen
+        self.flavor = flavor #flavor object
+        self.links = links # list of link objets
+
+    def add_node(self, node):
+        self.links.append(node)
