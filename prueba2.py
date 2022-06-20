8850d0584ceec82104af4caa6413f7acd0aef639
Jose - Laptop Acer <garcia.josea@pucp.edu.pe>
2022-06-19 20:45:16 -0500

    grafico de topologia listo

---
+++ prueba2.py
@@ -0,0 +1,4 @@
+
+network = {'nodos': {'n0': {'enlaces': ['n4']}, 'n1': {'enlaces': []}, 'n2': {'enlaces': ['n3', 'n4']}, 'n3': {'enlaces': []}, 'n4': {'enlaces': ['n0']}}, 'nombre': 'tel145'}
+
+            
